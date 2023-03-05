5e74b7f21e73a271ec121c54eba2532542818125
henriquecfaguiar <henriquecfaguiar@gmail.com>
2023-03-05 15:51:01 -0300

    Register routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', redirect: '/djs' },
+    { path: '/djs', component: null },
+    {
+      path: '/djs/:id',
+      component: null,
+      children: [{ path: 'contact', component: null }],
+    },
+    { path: '/register', component: null },
+    { path: '/requests', component: null },
+    { path: '/:notFound(.*)', component: null },
+  ],
 });
 
 export default router;
